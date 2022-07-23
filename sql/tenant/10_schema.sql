75bc1fd0888abc7e7aa8889417e968fdb08c82ae
ryoha000 <the7uya@gmail.com>
2022-07-23 16:49:26 +0900

    fix

--- sql/tenant/10_schema.sql
+++ sql/tenant/10_schema.sql
@@ -34,4 +34,3 @@ CREATE TABLE player_score (
 CREATE INDEX player_score_player_id_idx on player_score(player_id);
 CREATE INDEX player_score_competition_id_idx on player_score(competition_id);
 CREATE INDEX competition_tenant_id_created_at_idx on competition(tenant_id, created_at);
-CREATE INDEX competition_competition_id_player_id_idx on competition(competition_id, player_id);
