e96baaa76ce22803367ddaa19e41e8c3551e3532
sapphi-red <green@sapphi.red>
2022-07-23 15:55:52 +0900

    fix

--- sql/override.sh
+++ sql/override.sh
@@ -7,5 +7,5 @@ cd `dirname $0`
 
 for ((i=1 ; i < 101 ; i++)); do
 	echo running: $i
-  sqlite3 ../initial_data/$i.db < ./tenant/override.sql
+  sqlite3 ../../initial_data/$i.db < ./tenant/override.sql
 done
