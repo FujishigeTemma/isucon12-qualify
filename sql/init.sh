3132775caf718eb174d2f956933eff8d738ebbf9
sapphi-red <green@sapphi.red>
2022-07-23 13:01:31 +0900

    fix sql init

--- sql/init.sh
+++ sql/init.sh
@@ -20,7 +20,7 @@ mysql -u"$ISUCON_DB_USER" \
 rm -f ../tenant_db/*.db
 
 for ((i=0 ; i < 140 ; i++)); do
-	mysql -u"$ISUCON_DB_USER" \
+	mysql --max_allowed_packet=64M -u"$ISUCON_DB_USER" \
 			-p"$ISUCON_DB_PASSWORD" \
 			--host "$ISUCON_DB_HOST" \
 			--port "$ISUCON_DB_PORT" \
