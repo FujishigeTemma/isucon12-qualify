b29fb567dad273db4d7af29e35e1ad3c4d06da31
ryoha000 <the7uya@gmail.com>
2022-07-23 15:02:10 +0900

    add index

--- sql/tenant/10_schema.sql
+++ sql/tenant/10_schema.sql
@@ -32,3 +32,4 @@ CREATE TABLE player_score (
 );
 
 CREATE INDEX player_score_tenant_id_player_id_idx on player_score(tenant_id, player_id);
+CREATE INDEX competition_tenant_id_created_at_idx on competition(tenant_id, created_at);
