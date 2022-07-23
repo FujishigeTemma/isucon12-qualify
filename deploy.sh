1936868d268bcb714dde8dbc00cd9fb4f5986d32
FUJISHIGE TEMMA <tenma.x0@gmail.com>
2022-07-23 12:00:21 +0900

    :bug:fix sh

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,20 @@
 #!/bin/bash -eux
 
 function deploy () {
-  for file in "$(find /home/isucon/webapp/all -type f)"; do
-    sudo cp $file $(echo $file | sed "s/\/home\/isucon\/webapp\/all//")
+  for src in "$(find /home/isucon/webapp/all -type f)"; do
+    dest="$(echo $src | sed "s/\/home\/isucon\/webapp\/all//")"
+    sudo cp $src $dest 
   done
-  for file in "$(find /home/isucon/webapp/$HOSTNAME -type f)"; do
-    sudo cp $file $(echo $file | sed "s/\/home\/isucon\/webapp\/$HOSTNAME//")
+  for src in "$(find /home/isucon/webapp/$HOSTNAME -type f)"; do
+    dest=$(echo $src | sed "s/\/home\/isucon\/webapp\/$HOSTNAME//")
+    sudo cp $src $dest
   done
 
   (cd /home/isucon/webapp/go && go build -o app)
 
   sudo systemctl restart nginx
   sudo systemctl restart mysql
-  sudo systemctl restart isuports.go
+  sudo systemctl restart isuports.service
 
   sudo sysctl -p /etc/sysctl.d/99-isucon.conf
   
