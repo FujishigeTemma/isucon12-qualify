efcff386a2e6269df6971639aaf503212bf8da5c
sapphi-red <green@sapphi.red>
2022-07-23 12:28:05 +0900

    fix bash

--- sql/init.sh
+++ sql/init.sh
@@ -19,7 +19,7 @@ mysql -u"$ISUCON_DB_USER" \
 # SQLiteのデータベースを初期化
 rm -f ../tenant_db/*.db
 
-for (( i=0 ; i < 100 ; i++ )) do
+for (( i=0 ; i < 100 ; i++ )); do
 	mysql -u"$ISUCON_DB_USER" \
 			-p"$ISUCON_DB_PASSWORD" \
 			--host "$ISUCON_DB_HOST" \
