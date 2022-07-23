0b1d496e498e5142c91093bc79cf738b30934964
FUJISHIGE TEMMA <tenma.x0@gmail.com>
2022-07-23 10:58:31 +0900

    :zap:fix deploy.sh & add kernel params

--- deploy.sh
+++ deploy.sh
@@ -8,3 +8,31 @@ function deploy () {
     cp basepath/$file /${file#*/}
   done
 }
+#!/bin/bash -eux
+
+function deploy () {
+  basepath="/home/isucon/webapp" # To Be Changed
+
+  for file in "$(find all -type f)"; do
+    cp $basepath/$file /${file#*/}
+  done
+  for file in "$(find $HOSTNAME -type f)"; do
+    cp $basepath/$file /${file#*/}
+  done
+
+  (cd /home/isucon/webapp/go && go build -o app)
+
+  # sudo systemctl restart nginx
+  # sudo systemctl restart mysql
+  # sudo systemctl restart isucon.go
+
+  sudo sysctl -p /etc/sysctl.d/100-isucon.conf
+  
+  # refresh
+  source $basepath/deploy.sh
+}
+
+function sync () {
+  git fetch && \
+  git reset --hard origin/$1 # <username>/<branch_name>
+}
