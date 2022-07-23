400b63659c669b5758d66427b494435a6abc3615
ryoha000 <the7uya@gmail.com>
2022-07-23 14:53:59 +0900

    fix

--- sql/tenant/10_schema.sql
+++ sql/tenant/10_schema.sql
@@ -31,4 +31,4 @@ CREATE TABLE player_score (
   updated_at BIGINT NOT NULL
 );
 
-CREATE INDEX tenant_id_player_id_idx on player_score(tenant_id, player_id);
+CREATE INDEX player_score_tenant_id_player_id_idx on player_score(tenant_id, player_id);
