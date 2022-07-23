19b279b1e1f2a7fb534d799d7a0341efd8d7d98e
FUJISHIGE TEMMA <tenma.x0@gmail.com>
2022-07-23 11:21:39 +0900

    :bug:fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,35 +1,23 @@
-function deploy () {
-  server="$(hostname)"
-  basepath="/home/isucon/webapp" # Tobe changed
-  for file in "$(find all -type f)"; do
-    cp basepath/$file /${file#*/}
-  done
-  for file in "$(find $server -type f)"; do
-    cp basepath/$file /${file#*/}
-  done
-}
 #!/bin/bash -eux
 
 function deploy () {
-  basepath="/home/isucon/webapp" # To Be Changed
-
-  for file in "$(find all -type f)"; do
-    cp $basepath/$file /${file#*/}
+  for file in "$(find /home/isucon/webapp/all -type f)"; do
+    sudo cp $file $(echo $file | sed "s/\/home\/isucon\/webapp\/all//")
   done
-  for file in "$(find $HOSTNAME -type f)"; do
-    cp $basepath/$file /${file#*/}
+  for file in "$(find /home/isucon/webapp/$HOSTNAME -type f)"; do
+    sudo cp $file $(echo $file | sed "s/\/home\/isucon\/webapp\/$HOSTNAME//")
   done
 
   (cd /home/isucon/webapp/go && go build -o app)
 
-  # sudo systemctl restart nginx
-  # sudo systemctl restart mysql
-  # sudo systemctl restart isucon.go
+  sudo systemctl restart nginx
+  sudo systemctl restart mysql
+  sudo systemctl restart isucon.go
 
-  sudo sysctl -p /etc/sysctl.d/100-isucon.conf
+  sudo sysctl -p /etc/sysctl.d/99-isucon.conf
   
   # refresh
-  source $basepath/deploy.sh
+  source /home/isucon/webapp/deploy.sh
 }
 
 function sync () {
