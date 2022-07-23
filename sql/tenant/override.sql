fe5f17ce5c9d129cd73b32bce4cc068a59273764
sapphi-red <green@sapphi.red>
2022-07-23 16:21:20 +0900

    fix

--- sql/tenant/override.sql
+++ sql/tenant/override.sql
@@ -2,4 +2,4 @@ DROP INDEX player_score_tenant_id_player_id_idx;
 
 CREATE INDEX player_score_player_id_idx on player_score(player_id);
 CREATE INDEX player_score_competition_id_idx on player_score(competition_id);
-CREATE INDEX competition_tenant_id_created_at_idx on competition(tenant_id, created_at);
+
