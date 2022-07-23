067806c74bde2a8bb9971dc91c7074f5d1de989a
sapphi-red <green@sapphi.red>
2022-07-23 15:54:04 +0900

    fix

--- sql/override.sh
+++ sql/override.sh
@@ -5,7 +5,7 @@ cd `dirname $0`
 
 # SQLiteのデータベースの書きかえ
 
-for ((i=0 ; i < 140 ; i++)); do
+for ((i=1 ; i < 140 ; i++)); do
 	echo running: $i
   sqlite3 ../tenant_db/$i.db < ./tenant/override.sql
 done
